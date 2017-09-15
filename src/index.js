8bac8193fd1a09f30ba360518fc47609f779c163
alenadmit <dmitalyona@yandex.ru>
2017-09-15 22:25:19 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const   btn = document.querySelector("#button"),
+        res = document.querySelector("#response"),
+        src = document.querySelector("#source"),
+        author = document.querySelector("#author");
+
+btn.addEventListener("click", function() {
+    author.innerHTML = "Author: Alena Dmitrievskaia";
+    src.innerHTML = "Address for check: ";
+
+    let url = window.location.href.replace(/^.*?\?/, '').replace(/#.*$/, '').split('&');
+
+    (/^([a-z]{3,})@(gmail\.com)$/.test(url)) ? (res.innerHTML = "yes") : (res.innerHTML = "no");
+
+})
